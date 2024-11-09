7af1ee7f754ef867c45ffcf106066d2f8093e8e1
saira <saira.fatima@ucalgary.ca>
2024-11-09 15:07:29 -0700

    the user API route has been set up

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const userRoutes = require('./routes/users'); // Make sure the path is correct
+
 const app = express();
 
 // Middleware
@@ -13,6 +15,9 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Hello from backend!');
 });
+
+// Use the user routes for paths starting with '/api/users'
+app.use('/api/users', userRoutes);
 
 // Port
 const PORT = process.env.PORT || 5000;
